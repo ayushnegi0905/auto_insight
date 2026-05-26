e21501a60000b8448bb7367373b9b5f7764bee0c
ayushnegi0905 <ayushnegichinu0905@gmail.com>
2026-05-26 16:43:20 +0530

    Additional Commits

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,29 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes import upload
 from app.routes import auth
 
-from fastapi.middleware.cors import CORSMiddleware
-
-from app.db.database import engine,Base
+from app.db.database import engine, Base
 from app.db import models
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "AutoInsight API running"}
-
-app.include_router(upload.router)
-
-app.include_router(auth.router)
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://auto-insight-lovat.vercel.app/"],
+    allow_origins=["https://auto-insight-lovat.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+@app.get("/")
+def home():
+    return {"message": "AutoInsight API running"}
+
+app.include_router(upload.router)
+app.include_router(auth.router)
